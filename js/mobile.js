824464f15201e115736d3cfdab90f574b6ad0c77
‘PerfectLuo’ <848182483@qq.com>
2021-01-05 09:18:42 +0800

    Site updated: 2021-01-05 09:18:41

--- js/mobile.js
+++ js/mobile.js
@@ -16,7 +16,6 @@ define([], function(){
 	//第一步 -- 组合
 	var combine = function(){
 		if($tag){
-			console.log($tag)
 			document.getElementById("js-mobile-tagcloud").innerHTML = $tag.innerHTML;
 		}
 		if($aboutme){
